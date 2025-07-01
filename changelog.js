ff5279f939974952523ba5b11369bad7ce62689b
Tvalc <tony@glittercloud.solutions>
2025-06-30 18:00:30 -0700

    Update from Snib Studio - 2025-07-01T01:00:30.201Z

---
+++ changelog.js
@@ -0,0 +1,13 @@
+// changelog.js
+export const CHANGE_LOG = [
+  {
+    version: "v0.1.0",
+    date: "2024-06-08",
+    changes: [
+      "Project initialized.",
+      "Added basic game loop to maingame.js.",
+      "Set up index.html and style.css."
+    ]
+  }
+  // Add new entries here as the project evolves
+];
